7f034240f6de5475f9fe32256c66b5ce58b61f34
Cristian <cristian.burgi@gmail.com>
2024-07-07 20:54:50 -0300

    primer commit
    Subiendo los archivos a la plataforma

---
+++ src/main/java/com/cburgi/foro_project/ForoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cburgi.foro_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cburgi/foro_project/ForoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cburgi.foro_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
